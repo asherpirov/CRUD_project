79f472d4557d9350b8676733351168d77858617c
Asher Pirov <Asherpirov@gmail.com>
2026-05-27 10:30:59 +0200

    docs: add docstring to main function

---
+++ main.py
@@ -0,0 +1,20 @@
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format= "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+                    handlers= [
+                        logging.FileHandler("app.log", encoding="utf-8"),
+                        logging.StreamHandler()
+                    ]
+                )
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    """Execute the main program logic and log execution."""
+    logger.info("The program started running")
+
+
+if __name__ == "__main__":
+    main()
