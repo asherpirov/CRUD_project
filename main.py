a9ae3728be19d307d4d88c1dbeaa1c961ccc43f5
Asher Pirov <Asherpirov@gmail.com>
2026-05-27 11:28:54 +0200

    feat: initialize root logger and add startup log in main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
-
+from rectangle import Rectangle
 logging.basicConfig(level=logging.INFO,
-                    format= "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+                    format= "%(asctime)s | %(name)s | %(levelname)s | %(message)s",
                     handlers= [
                         logging.FileHandler("app.log", encoding="utf-8"),
                         logging.StreamHandler()
@@ -14,7 +14,6 @@ logger = logging.getLogger(__name__)
 def main():
     """Execute the main program logic and log execution."""
     logger.info("The program started running")
-
 
 if __name__ == "__main__":
     main()
