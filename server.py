b36ec807768ce51bb48a4d4a60252b1be9f1a6b4
Asher Pirov <Asherpirov@gmail.com>
2026-06-02 11:53:29 +0200

    add post path create shape to /shapes

--- server.py
+++ server.py
@@ -16,17 +16,25 @@ def all_shape():
 
 @app.get("/shapes/total-area")
 def get_total_area():
+    if not manager.shapes:
+        raise HTTPException(status_code=404, detail= "No shapes found calculate area")
     area = manager.get_total_area()
     return {"sum of total area" : round(area,2)}
 
 
 @app.get("/shapes/{id}")
 def get_shape_by_id(id: int):
-    try:
-        for shape in manager.shapes:
-            shape = shape.to_dict()
-            if shape["id"] == id:
-                return shape
-    except:
-        raise HTTPException(status_code=404,
-            detail="the shape ot exist")
+    for shape in manager.shapes:
+        shape = shape.to_dict()
+        if shape["id"] == id:
+            return shape
+    return []
+
+
+@app.post("/shapes", status_code=201)
+def create_shape(shape_dict: dict):
+    id = manager.get_new_id()
+    shape_dict["id"] = id
+    created_shape = manager.create_shape(shape_dict)
+    return created_shape.to_dict()
+
